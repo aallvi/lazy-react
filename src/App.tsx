9d93efa46ba3179318277fb1a85c2534c86db687
Alvaro <aleiva97@gmail.com>
2022-11-16 18:37:39 -0300

    proyecto base vite ts react sass react router v6'

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Navigation } from './routes/Navigation'
+import './styles/styles.scss'
+
+function App() {
+
+
+  return (
+  <>
+       <Navigation/>
+  </>
+  )
+}
+
+export default App
